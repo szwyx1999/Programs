a828ee49e77a0382aa1e1ebb04f2e92cec4b14c9
Yuxi <55152141+szwyx1999@users.noreply.github.com>
2020-12-08 23:18:09 -0700

    p4

---
+++ Circles/src/edu/nmsu/cs/circles/Circle2Test.java
@@ -0,0 +1,53 @@
+package edu.nmsu.cs.circles;
+
+import org.junit.*;
+
+public class Circle2Test
+{
+	// Data you need for each test case
+	private Circle1 circle2;
+
+	//
+	// Stuff you want to do before each test case
+	//
+	@Before
+	public void setup()
+	{
+		System.out.println("\nTest starting...");
+		circle2 = new Circle1(0, 0, 3);
+	}
+
+	//
+	// Stuff you want to do after each test case
+	//
+	@After
+	public void teardown()
+	{
+		System.out.println("\nTest finished.");
+	}
+
+	//
+	// Test a simple positive move
+	//
+	@Test
+	public void simpleMove()
+	{
+		Point p;
+		System.out.println("Running test simpleMove.");
+		p = circle2.moveBy(1, 1);
+		Assert.assertTrue(p.x == 1 && p.y == 1);
+	}
+
+	//
+	// Test a simple negative move
+	//
+	@Test
+	public void simpleMoveNeg()
+	{
+		Point p;
+		System.out.println("Running test simpleMoveNeg.");
+		p = circle2.moveBy(-1, -1);
+		Assert.assertTrue(p.x == -1 && p.y == -1);
+	}
+}
+
